4692ff1b76a23cc1589994dbdf3e0330e4bf4ad5
Shivaksk119 <shivapatelkarru119ksk@gmail.com>
2023-09-16 14:17:17 +0530

    first Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] ags) {
+
+        RWOnly obj = new RWOnly();
+
+        //obj.name = "Shiva";  Show error .name variable is not accessible in this class
+
+        obj.setName("Shiva");
+
+    }
   
 }
